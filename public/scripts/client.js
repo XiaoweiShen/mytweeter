d407c8c50424f08de2724ebfe65d3eb77788920a
XIAOWEI SHEN <labber@DESKTOP-NH2IC5D.localdomain>
2022-10-13 15:50:46 -0700

    move hover function from client.js to css file, simplier and clear

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,21 +4,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function() {
-  //highlight the footer element when mouse hover
-  $(".tweet-footer-right").children().mouseover(function(){
-    $(this).css("color",'orange');
-  }).mouseout(function(){
-    $(this).css("color",'');
-  })
-
-  //box shadow when mouse hover
-  $("article.tweet").mouseover(function(){
-    $(this).css("box-shadow","5px 10px #888888");
-  }).mouseout(function(){
-    $(this).css("box-shadow","");
-  })
-  
-  //counter color toggle due to count of chars
+  //hide or show the compose tweet when click
   $(".top-bar-right").click(function(){
     $(".new-tweet").toggle();
   })  
